66dd97421267c809bc462d1d98939399e87a92b5
hinx <b2765270@gmail.com>
2016-03-02 11:19:45 +0800

    remove user repo

--- src/main/java/org/lightadmin/boot/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.lightadmin.boot.repository;
-
-import org.lightadmin.boot.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
